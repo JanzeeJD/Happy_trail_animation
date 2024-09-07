81163e0f6b21a3f606c741ba23e3d6f862b8844b
Jancy Daniel <jancyjaya12@gmail.com>
2024-09-07 10:00:00 +0530

    chore: add emoji class to span for better styling control

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ bodyEl.addEventListener("mousemove", (event)=>{
     const xPos = event.offsetX;
     const yPos = event.offsetY;
     const spanEl = document.createElement("span");
+    spanEl.classList.add("emoji"); 
     spanEl.style.left = xPos + "px";
     spanEl.style.top = yPos + "px";
     const size = Math.random() * 100;
